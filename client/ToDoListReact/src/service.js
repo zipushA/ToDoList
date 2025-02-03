c64e3f2330a46ab1fb1fced6f79d4e80902e7906
zipushA <zh6737478@gmail.com>
2025-02-03 14:54:55 +0200

    nnnn

---
+++ client/ToDoListReact/src/service.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+axios.defaults.baseURL = "http://localhost:5023";
+
+// הוספת interceptor לשגיאות
+axios.interceptors.response.use(
+  response => response, // אם הבקשה הצליחה, מחזירים את התגובה
+  error => {
+    console.error('API Error:', error.response ? error.response.data : error.message);
+    return Promise.reject(error); // דחיית ההבטחה עם השגיאה
+  }
+);
+
+//const apiUrl = "http://localhost:5023"
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/items`)    
+    return result.data;
+  },
+
+  addTask: async(name)=>{
+    console.log('addTask', name)
+    const result = await axios.post(`/items`, { name });
+    return result.data;
+  },
+
+  // setCompleted: async(id, isComplete)=>{
+  //   console.log('setCompleted', { id, isComplete });
+  //   const updatedTask = { isComplete };
+  //   await axios.put(`/items/${id}`, updatedTask);
+  //   return { id, isComplete };
+  // },
+  setCompleted: async (id, name, isComplete) => {
+    console.log('setCompleted', { id, name, isComplete });
+    const updatedTask = { name, isComplete };
+    await axios.put(`/items/${id}`, updatedTask);
+    return { id, isComplete };
+  },
+  deleteTask:async(id)=>{
+    console.log('deleteTask')
+    const result = await axios.delete(`/items/${id}`);
+    return result.data;
+  }
+};
